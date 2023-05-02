7337865847efdc0ee3c4e8b24f5b71262df9f03a
Karina Boronina <Gubashina@yandex.ru>
2023-05-02 23:13:04 +0300

    Создан и добавлен компонент NotFoundPage, налажены роуты между ним и главной страницей

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,16 @@
-import React from "react";
-import "./main.css"
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import './main.css';
+import NotFoundPage from '../../pages/NotFoundPage/NotFoundPage';
+import PostsList from '../../pages/PostsList/PostsList';
 
 const Main = () => {
     return (
         <main className="main">
-            Я мейн
+            <Routes>
+                <Route path="/" element={<PostsList />}></Route>
+                <Route path="*" element={<NotFoundPage />}></Route>
+            </Routes>
         </main>
     );
 };
---
+++ src/pages/PostsList/PostsList.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './postslist.css';
+
+const PostsList = (props) => {
+    return <div className="container">Тут будут все посты</div>;
+};
+
+export default PostsList;
