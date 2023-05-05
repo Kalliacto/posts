d0dcb6c19e5077917ac96e85e90fefb36c6f5780
Karina Boronina <Gubashina@yandex.ru>
2023-05-05 10:49:44 +0300

    изменено использование кнопок

--- src/pages/PostsPage/PostsPage.jsx
+++ src/pages/PostsPage/PostsPage.jsx
@@ -2,13 +2,13 @@ import React, { useContext } from 'react';
 import './postsPage.css';
 import PostsList from '../../components/PostsList/PostsList';
 import { Context } from '../../context/Context';
-import SortPosts from '../../components/SortPosts/SortPosts';
+import PostsListTitle from '../../components/PostsListTitle/PostsListTitle';
 
 const PostsPage = () => {
     const { posts } = useContext(Context);
     return (
         <div className="container">
-            <SortPosts />
+            <PostsListTitle />
             <PostsList posts={posts} />
         </div>
     );
