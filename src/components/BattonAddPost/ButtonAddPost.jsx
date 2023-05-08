cb6900bd0ff35fcbbbffe3d6038d623367157ee8
Karina Boronina <Gubashina@yandex.ru>
2023-05-08 10:41:01 +0300

    добавлен компонент модального окна, есть открытие и закрытие

--- src/components/BattonAddPost/ButtonAddPost.jsx
+++ src/components/BattonAddPost/ButtonAddPost.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import './buttonaddpost.css';
 
 const ButtonAddPost = ({ ...props }) => {
-    const { title } = props;
+    const { title, addModal } = props;
+
     return (
         <>
             <button
                 className="button__add"
                 onClick={() => {
                     console.log('есть контакт!');
+                    addModal();
                 }}
             >
                 {title}
