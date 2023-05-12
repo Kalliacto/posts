740b64d3f91e515384873c7b27e7b48ad9b47f14
Karina Boronina <Gubashina@yandex.ru>
2023-05-12 11:02:03 +0300

    добавление удаления поста без отрисовки сразу

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -4,13 +4,23 @@ import { Chat, Heart, HeartFill } from 'react-bootstrap-icons';
 import { Link } from 'react-router-dom';
 import { Context } from '../../context/Context';
 import { likeToggle } from '../../utils/utils';
+import { Trash3 } from 'react-bootstrap-icons';
+import { api } from '../../api/api';
 
 const Post = ({ post }) => {
     const { user, setPosts } = useContext(Context);
     const { author, image, title, text, tags, likes, created_at, _id, comments } = post;
     const wasLiked = likes.includes(user._id);
+
+    const deletePost = (id, setPosts) => {
+        api.deletePostById(id).then((post) =>
+            setPosts((state) => state.filter((post) => post._id !== id))
+        );
+    };
+
     return (
         <div className='post'>
+            <Trash3 className='post__trash' onClick={() => deletePost(post._id)} />
             <Link to={`/profile/${author._id}`}>
                 <div className='post__header'>
                     <img src={author.avatar} alt='avatar' className='post__autor-avatar' />
